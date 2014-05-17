ee8d56610f948ca08111ad54c0e201ca0ad11d6d
abellini <alexandrebli@gmail.com>
2014-05-16 22:05:02 -0300

    Create Food bean and Repository.

--- activity/src/main/java/br/com/flygowmobile/database/RepositoryFood.java
+++ activity/src/main/java/br/com/flygowmobile/database/RepositoryFood.java
@@ -1,6 +1,7 @@
 package br.com.flygowmobile.database;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.provider.BaseColumns;
 
@@ -9,6 +10,8 @@ import java.util.List;
 import br.com.flygowmobile.entity.Food;
 
 public class RepositoryFood extends Repository<Food> {
+
+    private static final String REPOSITORY_FOOD = "RepositoryFood";
 
     public static abstract class Foods implements BaseColumns {
 
@@ -27,9 +30,24 @@ public class RepositoryFood extends Repository<Food> {
 
     }
 
+    public RepositoryFood(Context ctx) {
+        db = ctx.openOrCreateDatabase(RepositoryScript.DATABASE_NAME, Context.MODE_PRIVATE, null);
+    }
+
     @Override
-    public long save(Food obj) {
-        return 0;
+    public long save(Food food) {
+
+        long id = food.getFoodId();
+
+        Food f = findById(id);
+        if (f != null) {
+            if (f.getFoodId() != 0) {
+                this.update(food);
+            }
+        } else {
+            id = this.insert(food);
+        }
+        return id;
     }
 
     @Override
--- activity/src/main/java/br/com/flygowmobile/entity/Food.java
+++ activity/src/main/java/br/com/flygowmobile/entity/Food.java
@@ -1,4 +1,84 @@
 package br.com.flygowmobile.entity;
 
+import br.com.flygowmobile.database.RepositoryFood;
+
 public class Food {
+
+    public static String[] columns = new String[] {
+            RepositoryFood.Foods.COLUMN_NAME_FOOD_ID,
+            RepositoryFood.Foods.COLUMN_NAME_NAME,
+            RepositoryFood.Foods.COLUMN_NAME_VALUE,
+            RepositoryFood.Foods.COLUMN_NAME_DESCRIPTION,
+            RepositoryFood.Foods.COLUMN_NAME_NUTRITIONAL_INFO,
+            RepositoryFood.Foods.COLUMN_NAME_IS_ACTIVE,
+            RepositoryFood.Foods.COLUMN_NAME_CATEGORY_ID
+    };
+
+    private long foodId;
+    private String name;
+    private Double value;
+    private String description;
+    private String nutritionalInfo;
+    private boolean isActive;
+    private int categoryId;
+
+    public Food(){
+
+    }
+
+    public long getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(long foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNutritionalInfo() {
+        return nutritionalInfo;
+    }
+
+    public void setNutritionalInfo(String nutritionalInfo) {
+        this.nutritionalInfo = nutritionalInfo;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
 }
